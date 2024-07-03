3eef6a15c32d5b610f7e7186b202447bb7c85570
Bruno Pantaleão <bruno.ing879@gmail.com>
2024-07-03 13:50:16 +0200

    Initial Commit

---
+++ Improv-iOS/Improv_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Improv_iOS.h
+//  Improv-iOS
+//
+//  Created by Bruno Pantaleão on 03/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Improv_iOS.
+FOUNDATION_EXPORT double Improv_iOSVersionNumber;
+
+//! Project version string for Improv_iOS.
+FOUNDATION_EXPORT const unsigned char Improv_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Improv_iOS/PublicHeader.h>
+
+
